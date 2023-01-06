905e73882722edfac3d2399febf1de3d87d0b1c3
Daniel Acosta <da_co@hotmail.es>
2023-01-06 09:11:00 -0600

    Connect APIs to DBs

--- api_fcsh/src/app.module.ts
+++ api_fcsh/src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'database',
+      port: 5432,
+      username: 'uaslp_fcsh',
+      password: 'admin',
+      database: 'uaslp_fcsh',
+      entities: [],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
